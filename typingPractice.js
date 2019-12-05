683d9e9a124e2dfc26808362f81b3908bfded52e
ayush <58028488+desibabua@users.noreply.github.com>
2019-12-05 11:39:40 +0530

    added feature of colors and options for --w or --c

--- typingPractice.js
+++ typingPractice.js
@@ -1,41 +1,42 @@
 let { stdin, stdout } = process;
 stdin = stdin.setEncoding("utf8");
-let initialchar = "abcdefghijklmnopqrstuvwxyzx".split("").map((char)=>char+"\n")
+let words = require("./words.json");
+let initialchar = "abcdefghijklmnopqrstuvwxyzx".split("");
+const chalk = require("chalk");
 
-let Characters = [
-  {
-    char: "a\n",
-    x: 100,
-    y: 2
-  }
-];
+const { blue, yellowBright, green, white, red } = chalk;
+
+let colors = [blue, yellowBright, green, white, red];
+let characters = [];
 
 const newCharMaker = function() {
-  let info = {};
-  let columns = process.stdout.columns;
-  Math.floor(Math.random() * 26);
-  info.char = initialchar[Math.floor(Math.random() * 26)];
-  info.x = Math.floor(Math.random() * columns);
-  info.y = 2;
-  return info;
+  let charcterInfo = {};
+  let columns = process.stdout.columns - 5;
+  let multiplier = initialchar.length;
+  const selectedColumn = Math.floor(Math.random() * multiplier);
+  charcterInfo.char = initialchar[selectedColumn];
+  charcterInfo.x = Math.floor(Math.random() * columns);
+  charcterInfo.y = 2;
+  charcterInfo.color = colors[Math.floor(Math.random()*5)]
+  return charcterInfo;
 };
 
 const characterInserter = function() {
-  Characters.push(newCharMaker());
+  characters.push(newCharMaker());
 };
 
 const printChars = function() {
-  process.stdout.cursorTo(0, 1);
-  process.stdout.clearScreenDown();
-  Characters.forEach(charinfo => {
-    process.stdout.cursorTo(charinfo.x, charinfo.y);
-    console.log(charinfo.char);
+  stdout.cursorTo(0, 1);
+  stdout.clearScreenDown();
+  characters.forEach(charinfo => {
+    stdout.cursorTo(charinfo.x, charinfo.y);
+    console.log(charinfo.color(charinfo.char));
     charinfo.y += 1;
   });
 };
 
 const isGameOver = function(rows) {
-  Characters.forEach(charInfo => {
+  characters.forEach(charInfo => {
     if (charInfo.y == rows) {
       process.exit(0);
     }
@@ -43,19 +44,30 @@ const isGameOver = function(rows) {
 };
 
 const envSetUp = function() {
-  let rows = process.stdout.rows;
+  let rows = stdout.rows;
   printChars();
   isGameOver(rows);
-  process.stdout.cursorTo(0, 37);
+  stdout.cursorTo(0, 35);
+};
+
+const wordsMaker = function() {
+  if (process.argv[2] == "--c") {
+    stdin.setRawMode("true");
+  }
+  if (process.argv[2] == "--w") {
+    initialchar = initialchar.concat(words);
+    initialchar = initialchar.map(element => element + "\n");
+  }
 };
 
 const main = function() {
-  setInterval(envSetUp, 1000);
-  setInterval(characterInserter, 1500);
+  wordsMaker();
+  setInterval(envSetUp, 500);
+  setInterval(characterInserter, 1000);
   stdin.on("data", inputChar => {
-    Characters.forEach(charinfo => {
+    characters.forEach(charinfo => {
       if (inputChar == charinfo.char) {
-        Characters.splice(Characters.indexOf(charinfo), 1);
+        characters.splice(characters.indexOf(charinfo), 1);
       }
     });
   });
